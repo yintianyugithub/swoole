80f3a8f6f58f7ef5af3b900f10a88d4454bd8423
yintianyu <yintianyu@qq.com>
2020-03-11 11:39:44 +0800

    httpserver

--- httpServer.php
+++ httpServer.php
@@ -1,6 +1,12 @@
 <?php
 $http = new Swoole\Http\Server("0.0.0.0", 9501);
 $http->on('request', function ($request, $response) {
-	    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+    if ($request->server['path_info'] == '/favicon.ico' || $request->server['request_uri'] == '/favicon.ico'){
+        $response->end();
+        return;
+    }
+    var_dump($request->get,$request->post);
+    $response->header("Content-Type", "text/html; charset=utf-8");
+    $response->end("this is test of http server". rand(1000,9999));
 });
 $http->start();
