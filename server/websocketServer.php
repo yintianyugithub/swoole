ca7fdbfece4226d18f473c04625911b772142b79
yintianyu <yintianyu@qq.com>
2020-03-11 18:21:02 +0800

    m

--- server/websocketServer.php
+++ server/websocketServer.php
@@ -1,34 +1,22 @@
 <?php
+//创建websocket服务器对象，监听0.0.0.0:9502端口
+$ws = new Swoole\WebSocket\Server("0.0.0.0", 9502);
 
-class websocketServer
-{
-    public $server;
+//监听WebSocket连接打开事件
+$ws->on('open', function ($ws, $request) {
+    var_dump($request->fd, $request->get, $request->server);
+    $ws->push($request->fd, "hello, welcome\n");
+});
 
-    public function __construct()
-    {
-        $this->server = new Swoole\WebSocket\Server('0.0.0.0',9502);
-        $this->server->on('open', function (Swoole\WebSocket\Server $server, $request) {
-            echo "server: handshake success with fd{$request->fd}\n";
-        });
-        $this->server->on('message', function (Swoole\WebSocket\Server $server, $frame) {
-            echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-            $server->push($frame->fd, "this is server");
-        });
-        $this->server->on('close', function ($ser, $fd) {
-            echo "client {$fd} closed\n";
-        });
-        $this->server->on('request', function ($request, $response) {
-            // 接收http请求从get获取message参数的值，给用户推送
-            // $this->server->connections 遍历所有websocket连接用户的fd，给所有用户推送
-            foreach ($this->server->connections as $fd) {
-                // 需要先判断是否是正确的websocket连接，否则有可能会push失败
-                if ($this->server->isEstablished($fd)) {
-                    $this->server->push($fd, $request->get['message']);
-                }
-            }
-        });
-        $this->server->start();
-    }
-}
+//监听WebSocket消息事件
+$ws->on('message', function ($ws, $frame) {
+    echo "Message: {$frame->data}\n";
+    $ws->push($frame->fd, "server: {$frame->data}");
+});
 
-new websocketServer();
+//监听WebSocket连接关闭事件
+$ws->on('close', function ($ws, $fd) {
+    echo "client-{$fd} is closed\n";
+});
+
+$ws->start();
