ab1205de259d5bdcf9707f469000f4a23a63aae8
yintianyu <yintianyu@qq.com>
2020-03-12 11:44:01 +0800

    m

--- server/websocketServer.php
+++ server/websocketServer.php
@@ -6,19 +6,34 @@ class websocketServer
 
     public function __construct()
     {
+
         $this->server = new Swoole\WebSocket\Server('0.0.0.0',9502);
+        /**
+         * 客户端与服务端握手完成后回调
+         */
         $this->server->on('open', function (Swoole\WebSocket\Server $server, $request) {
             echo "server: handshake success with fd{$request->fd}\n";
+            $server->push($request->header['host']);
         });
+
+        /**
+         * 服务器接收客户端的数据帧后回调
+         */
         $this->server->on('message', function (Swoole\WebSocket\Server $server, $frame) {
             echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
             $server->push($frame->fd, "this is server");
         });
+
+        /**
+         * 客户端关闭连接
+         */
         $this->server->on('close', function ($ser, $fd) {
             echo "client {$fd} closed\n";
         });
+
         $this->server->on('request', function ($request, $response) {
             // 接收http请求从get获取message参数的值，给用户推送
+            $response->end("hello" . $request->header['x-real-ip']);
             // $this->server->connections 遍历所有websocket连接用户的fd，给所有用户推送
             foreach ($this->server->connections as $fd) {
                 // 需要先判断是否是正确的websocket连接，否则有可能会push失败
