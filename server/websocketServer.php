23df9e345911eeed5f40a2352d72ebd307545e65
yintianyu <yintianyu@qq.com>
2020-03-13 17:06:56 +0800

    http服务器分发任务到websocket

--- server/websocketServer.php
+++ server/websocketServer.php
@@ -34,7 +34,6 @@ class websocketServer
      */
     function onOpen($server, $request)
     {
-        var_dump($server);
         echo "server: handshake success with fd{$request->fd}\n";
     }
 
@@ -69,11 +68,6 @@ class websocketServer
      */
     function onRequest($request, $respose)
     {
-//        var_dump($request);
-//        var_dump($this->server);
-        // 接收http请求从get获取message参数的值，给用户推送
-        var_dump($this->server->get['message']);
-        // $this->server->connections 遍历所有websocket连接用户的fd，给所有用户推送
         foreach ($this->server->connections as $fd) {
             // 需要先判断是否是正确的websocket连接，否则有可能会push失败
             if ($this->server->isEstablished($fd) && $request->server['request_uri'] != '/favicon.ico') {
