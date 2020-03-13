c2cace2dc159eda8993cc038b7de7a8a735a5504
yintianyu <yintianyu@qq.com>
2020-03-13 12:47:00 +0800

    socketServer

--- server/websocketServer.php
+++ server/websocketServer.php
@@ -11,7 +11,7 @@ class websocketServer
         /**
          * 客户端与服务端握手完成后回调
          */
-        $this->server->on('open',['$this','myOnOpen']);
+        $this->server->on('open',[$this,'myOnOpen']);
 
         /**
          * 服务器接收客户端的数据帧后回调
