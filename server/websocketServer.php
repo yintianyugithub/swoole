a3af2415e42e3d5285a6dec3febc4b701193aadf
yintianyu <yintianyu@qq.com>
2020-03-13 14:10:49 +0800

    socketServer

--- server/websocketServer.php
+++ server/websocketServer.php
@@ -59,14 +59,14 @@ class websocketServer
      */
     function onRequest($request,$respose)
     {
+        var_dump($request);
+        var_dump($this->server);
         // 接收http请求从get获取message参数的值，给用户推送
-
+        var_dump($this->server->get['message']);
         // $this->server->connections 遍历所有websocket连接用户的fd，给所有用户推送
         foreach ($this->server->connections as $fd) {
-            var_dump($respose);
-            $respose->push("hello {$fd}");
             // 需要先判断是否是正确的websocket连接，否则有可能会push失败
-            if ($this->server->isEstablished($fd)) {
+            if ($this->server->isEstablished($fd) && $request->server['request_uri'] != '/favicon.ico') {
                 $this->server->push($fd, $request->get['message']);
             }
             $respose->header('Content-Type','text/html; charset=utf-8',true);
